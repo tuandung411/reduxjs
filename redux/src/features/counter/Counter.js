1edb191c8f9f02bb9ff5e57adcaf4b5b53429f1d
tuandung411 <tuandungpham2000@gmail.com>
2021-10-29 10:08:01 +0700

    status

---
+++ redux/src/features/counter/Counter.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    decrement,
+    increment,
+    incrementByAmount,
+    incrementAsync,
+    selectCount,
+} from "./counterSlice";
+import styles from "./Counter.module.css";
+
+export function Counter() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
+        </div>
+    );
+}
